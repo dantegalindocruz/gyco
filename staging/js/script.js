9a82d1e77710b5ac5a1031c51783c73e24e6e7b2
Dante Galindo Cruz <galindodantem@gmail.com>
2018-08-16 00:47:34 -0400

    Removing css from js and replacing it with classes to toggle

--- staging/js/script.js
+++ staging/js/script.js
@@ -5,94 +5,55 @@ var hamburgerMenuIcon = document.querySelectorAll('#hamburger-icon div');
 
 hamburgerMenu.addEventListener('click', (event) => {
        event.preventDefault();
-      if(links.style.left==="" || links.style.left==="-900px"){
-        showMenu(links);
+      if(links.className === 'hideMenu'){
+        links.className = 'showMenu';
         changeIconColor(hamburgerMenuIcon);
       } else{
-        hideMenu(links);
+        links.className = 'hideMenu';
         resetIconColor(hamburgerMenuIcon);
       }
 
 });
 
 hamburgerMenu.addEventListener('blur', () => {
-  if(links.style.left === '0px'){
-       hideMenu(links);
+  if(links.className === 'showMenu'){
+       links.className = 'hideMenu';
        resetIconColor(hamburgerMenuIcon);
      }
 });
 
-function showMenu(node) {
-  node.style.left = 0;
-  node.style.transition = '.5s ease';
-}
-
-function hideMenu(node) {
-  node.style.left='-900px';
-  node.style.transition='.5s ease';
-}
-
 function changeIconColor(nodeList){
   for(var i =0; i < nodeList.length; i++){
-    nodeList[i].style.border= '1px solid #E682FB';
+    nodeList[i].className = 'changeIconColor';
   }
 }
 
 function resetIconColor(nodeList){
   for(var i =0; i < nodeList.length; i++){
-    nodeList[i].style.border= '1px solid #fff';
+    nodeList[i].className= 'resetIconColor';
   }
 }
 
 
 /************************ Slide Show ********************************/
-var imageArray = [];
-var imgNodeList = document.querySelectorAll('#slider .slider-img');
+var imgNodeList = document.querySelectorAll('#slider img');
 var incrementer = 0;
-//Hides the current images
-function hideImages(imgArray){
-  imgArray.forEach(img => {
-     img.style.display = 'none';
-  });
-}
 
-//converts nodeList into an array
-function createArray(nodeList){
-  var tempArray = [];
-  for(var i= 0; i < nodeList.length; i++){
-    tempArray[i] = nodeList[i];
-  }
-
-  return tempArray;
-}
-
-//remove previous image from slideShow
-function hidePreviousImage(img){
-  img.style.display= 'none';
-}
 
 //Starts the slide show  TODO: transition doesn't work on display, so use a combination of display and opacity for slideshow
-function slideShow(imgArray){
- if(incrementer < imgArray.length){
-   if(incrementer === 0){
-     imgArray[incrementer].style.display='block';
-     imgArray[incrementer].style.transition='display 2s';
-   } else{
-     hidePreviousImage(imgArray[incrementer-1]);
-     imgArray[incrementer].style.display='block';
-     imgArray[incrementer].style.transition='display 2s';
-   }
+function slideShow(nodeList){
+ if(incrementer < nodeList.length){
+     if(incrementer > 0){
+     nodeList[incrementer-1].className = 'notActive'
+     nodeList[incrementer].className = 'active';
+     }
  } else{
-    hidePreviousImage(imgArray[incrementer-1]);
+    nodeList[incrementer-1].className = 'notActive';
     incrementer = 0;
-    imgArray[incrementer].style.display='block';
+    nodeList[incrementer].className='active';
  }
 incrementer++;
-setTimeout(() => {slideShow(imageArray)}, 2000);
+setTimeout(() => {slideShow(nodeList)}, 2000);
 }
 
-
-
-imageArray = createArray(imgNodeList);
-hideImages(imageArray);
-slideShow(imageArray);
+slideShow(imgNodeList);
